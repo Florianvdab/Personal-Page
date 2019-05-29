91ddd753ec47d5d120d337e40f442a54ad733b39
Florianvdab <florian.vdab@gmail.com>
2019-05-29 10:45:02 +0200

    Add JS, CSS, CNAME

---
+++ public/javascripts/main.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", init)
+function init() {
+    for (let i = 0; i < document.querySelectorAll("li,a").length; i++) {
+        console.log('working...')
+        document.querySelectorAll("li,a")[i].addEventListener("click", activate)
+    }
+    function activate() {
+        document.getElementsByClassName("is-active")[0].className = "";
+        this.className = "is-active";
+    }
+}
