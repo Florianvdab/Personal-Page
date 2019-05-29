f8f21d8841cc2cb9138ea84842546a1661cf9199
Florianvdab <florian.vdab@gmail.com>
2019-05-29 12:36:54 +0200

    Social Media Page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", init)
+
 function init() {
     for (let i = 0; i < document.querySelectorAll("li,a").length; i++) {
         console.log('working...')
@@ -9,13 +10,30 @@ function init() {
         this.className = "is-active";
     }
 
-    // document.getElementById("home").addEventListener("click",)
+    document.getElementById("home").addEventListener("click", home);
+    document.getElementById("socialmedia").addEventListener("click", socialmedia);
+    document.getElementById("contact").addEventListener("click", contact);
+    document.getElementById("resume").addEventListener("click", resume);
 }
 
-function home(){
+function home() {
+    document.getElementById("main").innerHTML = '<p class="title is-size-2">Florian Vandenabeele</p >' +
+        '<p class="subtitle is-size-4">Student Software & System Engineer @ Howest Brugge</p>'
+}
+
+function socialmedia() {
+    document.getElementById("main").innerHTML = '<p class="title is-size-2">Social Media</p >' +
+        '<p class="subtitle is-size-4">Feel free to contact me!</p>'+
+        '<div><a href="https://www.facebook.com/florian.vandenabeele.35"><svg class="facebook"></svg></a>' +
+        '<a href="https://github.com/Florianvdab"><svg class="github"></svg></a>' +
+        '<a href="https://www.instagram.com/florian.vdab/"><svg class="instagram"></svg></a>' +
+        '<a href="https://steamcommunity.com/id/WifixOfficial/"><svg class="steam"></svg></a>'+
+        '</div>'
 
 }
 
-function socialmedia(){
-    
+function contact() {
+}
+
+function resume() {
 }
